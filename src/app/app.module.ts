0bc2c8f2aa86d4e40549fe2218b07a94868a3f55
Ricardo Wolff <ricfwolff@yahoo.com.br>
2018-04-24 13:37:55 -0300

    Changing styles from material design to bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatDividerModule} from '@angular/material';
+// import { MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatDividerModule} from '@angular/material';
 import {FlexLayoutModule} from "@angular/flex-layout";
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { CityWeatherCardComponent } from './city-weather-card/city-weather-card.component';
@@ -15,12 +15,13 @@ import { CityWeatherCardComponent } from './city-weather-card/city-weather-card.
   ],
   imports: [
     BrowserModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatDividerModule,
-    FlexLayoutModule
+    // MatButtonModule,
+    // MatCheckboxModule,
+    // MatCardModule,
+    // MatToolbarModule,
+    // MatDividerModule,
+    FlexLayoutModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
